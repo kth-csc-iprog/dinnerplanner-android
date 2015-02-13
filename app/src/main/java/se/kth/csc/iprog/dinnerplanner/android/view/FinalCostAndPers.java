8d1c13dea69a58001ca7f825c3fce4b69749e20e
katrinnilsson <nilsson.katrin@gmail.com>
2015-02-13 14:23:51 +0100

    Adding value to views and fixing some design

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/view/FinalCostAndPers.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/view/FinalCostAndPers.java
@@ -3,6 +3,8 @@ package se.kth.csc.iprog.dinnerplanner.android.view;
 import android.view.View;
 import android.widget.TextView;
 import se.kth.csc.iprog.dinnerplanner.android.R;
+import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+import se.kth.csc.iprog.dinnerplanner.model.Dish;
 
 /**
  * Created by Katrin on 2015-02-11.
@@ -10,12 +12,28 @@ import se.kth.csc.iprog.dinnerplanner.android.R;
 public class FinalCostAndPers {
 
     View view;
+    DinnerModel model;
 
-    public FinalCostAndPers(View view) {
+    public FinalCostAndPers(View view, DinnerModel model) {
 
         this.view = view;
         TextView totalCost = (TextView) view.findViewById(R.id.total_cost);
         TextView pers = (TextView) view.findViewById(R.id.pers);
+
+        // The following solution of for loops is temporary, for testing the layout, since only two dishes exist right now.
+        for (Dish dish : model.getDishesOfType(1)) {
+            model.addDishToMenu(dish);
+            break;
+        }
+        for (Dish dish : model.getDishesOfType(2)) {
+            model.addDishToMenu(dish);
+            break;
+        }
+
+        totalCost.setText("Total cost: " + model.getTotalMenuPrice() + " kr");
+        // Temporary solution for layout demonstration
+        model.setNumberOfGuests(4);
+        pers.setText(model.getNumberOfGuests() + " pers");
 
     }
 
